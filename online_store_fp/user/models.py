723ff06c7b5b8f04d17e97d91ce9379caaaa6c1e
Mihhail Tsaplogin <914283@gmail.com>
2020-04-25 14:28:58 +0300

    modified user models.py added accounts urls.py, forms.py

--- online_store_fp/user/models.py
+++ online_store_fp/user/models.py
@@ -7,13 +7,11 @@ class User(models.Model):
     name = models.CharField(max_length=60)
     email = models.EmailField(max_length=254)
     phone_number = models.BigIntegerField(unique=True)
-    id_code = models.CharField(max_length=50, blank=True, null=True)
     city = models.CharField(max_length=30)
     country = models.CharField(max_length=30)
     street = models.CharField(max_length=30)
     zip_code = models.CharField(max_length=10)
-    dt_entrance = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return "{} ({})".format(self.name, self.id_code)
+        return "{} ({})".format(self.name, self.login)
 
