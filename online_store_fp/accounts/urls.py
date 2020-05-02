2401803578ae79b819144203389c4b73a17de776
Mihhail Tsaplogin <914283@gmail.com>
2020-05-02 12:03:14 +0300

    added users app, views.py, urls.py, admin.py, templates for it

--- online_store_fp/accounts/urls.py
+++ online_store_fp/accounts/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 
-from .views import user_login, user_logout, user_signup, UserProfile
+from . import views
 
 urlpatterns = [
-    path('login/', user_login, name="user-login"),
-    path('logout/', user_logout, name="user-logout"),
-    path('signup/', user_signup, name="user-signup"),
-    path('user-profile/<int:pk>', UserProfile.as_view(), name="user-profile"),
+    path('', views.index, name='index'),
 ]
-
